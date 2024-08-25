3e256747ef40d77a23bf964059807681ca399fc0
mitsuotaro <m.angelocrisostomo@gmail.com>
2024-08-25 15:34:03 +0800

    done

--- guessGame.js
+++ guessGame.js
@@ -1,11 +1,11 @@
-let target        = Math.floor(Math.random() * 2) + 1;
-let guessNumber   = document.querySelector("#guessNumber");
-let getButtons    = document.querySelectorAll(".button");
-let playerBet     = document.querySelector("#bet");
-let betButton     = document.querySelector("#betButton");
+let target           = Math.floor(Math.random() * 2) + 1;
+let guessNumber      = document.querySelector("#guessNumber");
+let getButtons       = document.querySelectorAll(".button");
+let playerBet        = document.querySelector("#bet");
+let betButton        = document.querySelector("#betButton");
 let creditsRemaining = document.querySelector("#checkCredits").innerHTML;
-let earnedCredits = 10;
-let betCount      = 0;
+let earnedCredits    = 10;
+let betCount         = 0;
 
 for (let i = 0; i < getButtons.length; i++){
     document.querySelectorAll(".button")[i].addEventListener('click', function(){
@@ -21,7 +21,7 @@ function addBet(){
     if(creditsRemaining <= 0){
         alert("no more credits");
     } else{
-    playerBet.innerText     = `Bet: ${betCount +=2}`;
+    playerBet.innerText    = `Bet: ${betCount +=2}`;
     checkCredits.innerText = earnedCredits -= 2;
     };
 };
@@ -34,19 +34,19 @@ function game(key){
     if(betCount === 0){
         alert("No bet");
     } else{
-            target                  = Math.floor(Math.random() * 2) + 1;
+            target = Math.floor(Math.random() * 2) + 1;
             console.log(`target: ${target}`);
         if(target == key){
-            document.querySelector("h1").innerText="You win!";
-            checkCredits.innerText = (earnedCredits += (betCount * 2));
+            document.querySelector("h1").innerText        = "You win!";
+                                   checkCredits.innerText = (earnedCredits += (betCount * 2));
             setTimeout(showNumber, 100);
             playerBet.innerText = "Bet: 0";
-            betCount = 0;
+            betCount            = 0;
         } else{
-            document.querySelector("h1").innerText="You lose..";
+            document.querySelector("h1").innerText = "You lose..";
             setTimeout(showNumber, 100);
             playerBet.innerText = "Bet: 0";
-            betCount = 0;
+            betCount            = 0;
         };
     };
 };
@@ -55,7 +55,7 @@ function showNumber(key){
     guessNumber.innerText = target;
     setTimeout(hideNumber, 2000);
     function hideNumber(){
-        guessNumber.innerText="?";
+        guessNumber.innerText = "?";
     }
 };
 
